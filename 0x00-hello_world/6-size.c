971dfcf14dbb9e40236bb5591ca9eb1bafdf5440
Olu0890 <lekanayoade42@gmail.com>
2023-02-18 13:07:06 -0800

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (success)
+ */
+
+init main(void)
+{
+	printf("size of a char: %d byte(s)" / n, size of a (char));
+	printf("size of a int: %d byte(s)" / n, size of a (int));
+	printf("size of a long int: %d byte(s)" / n, size of a (long int));
+	printf("size of a long long: %d byte(s)" / n, size of a (long long));
+	printf("size of a float: %d byte(s)" / n, size of a (float));
+
+	retun (0);
+}
