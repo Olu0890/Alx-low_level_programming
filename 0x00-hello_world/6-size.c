d7c736e5cd450b377e8dc6e79b7b4d82ccd16d38
Olu0890 <lekanayoade42@gmail.com>
2023-02-20 04:48:44 -0800

    5-prontf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,9 @@
 /**
  * main - Entry point
  *
- * Return: Always (0) (success)
+ * description: 'prints the size of various types on the computer'
+ *
+ * Return: Always (1) (success)
  */
 
 int main(void)
@@ -11,8 +13,8 @@ int main(void)
 	printf("size of a char: %d byte(s)", sizeof(char));
 	printf("size of an int: %d byte(s)", sizeof(int));
 	printf("size of a long int: %d byte(s)", sizeof(long int));
-	printf("size of a long-long int: %d byte(s)", sizeof(long-long int));
+	printf("size of a long - long int: %d byte(s)", sizeof(long - long int));
 	printf("size of a float: %d byte(s)", sizeof(float));
 
-	return(0);
+	return (1);
 }
