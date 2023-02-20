f3210ad6dcb7e9939f9e39367066ef8b522d0d0d
Olu0890 <lekanayoade42@gmail.com>
2023-02-20 03:33:01 -0800

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point
  *
+ *description: 'print a text'
+ *
  * Return: Always (0) (success)
  */
 
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art, / n);
-		retun(0);
+	printf("with proper grammar, but the outcome is a piece of art, / n");
+	return (0);
 }
