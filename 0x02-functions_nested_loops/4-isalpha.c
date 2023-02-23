d2d317b149eb8ed8f02804c0cace6cfe418289dd
Olu0890 <lekanayoade42@gmail.com>
2023-02-22 22:39:28 -0800

    Islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the alphabet
+ *
+ * Return: success 1
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
